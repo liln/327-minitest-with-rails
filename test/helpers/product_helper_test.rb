ed0d15ba2b557055c56ffc4d487222138c17e604
Lillian Ng <lillian.k.ng@gmail.com>
2014-01-15 22:19:51 -0800

    added example skip tests

--- test/helpers/product_helper_test.rb
+++ test/helpers/product_helper_test.rb
@@ -4,4 +4,10 @@ describe ProductsHelper do
   it "converts number to currency" do
     number_to_currency(5).must_equal "$5.00"
   end
+
+  it "is skipped"
+
+  it "is also skipped" do
+    skip "Do this later"
+  end
 end
